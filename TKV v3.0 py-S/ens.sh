a239fab2f28396dab6d980617e33f2deb098b882
jon <jonsdlglf@gmail.com>
2026-03-22 20:59:27 +0100

    Delete TKV v3.0 py-S directory

--- TKV v3.0 py-S/ens.sh
+++
@@ -1,6 +0,0 @@
-wine nasm.exe apps/main.asm -o C/main.bin
-wine nasm.exe apps/cursor.asm -o C/cursor.bin -I apps/spr
-wine nasm.exe apps/barr.asm -o C/barr.bin
-
-echo "you need wine and nasm"
-echo "/nasm <- in root"
